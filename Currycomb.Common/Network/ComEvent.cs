5d48fb649e633bd860a377c1b6e8ab5ddeb6b441
Nick Kennedy <nickkennedy334@gmail.com>
2021-08-07 13:07:58 -0400

    fix: grab UtcNow as DateTimeOffset directly, instead of casting to it

--- Currycomb.Common/Network/ComEvent.cs
+++ Currycomb.Common/Network/ComEvent.cs
@@ -10,14 +10,14 @@ namespace Currycomb.Common.Network
 
         public ComEvent(EventType @event, object payload)
         {
-            Timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds();
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
             Event = @event.ToString();
             Payload = payload;
         }
 
         public ComEvent(EventType @event)
         {
-            Timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds();
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
             Event = @event.ToString();
             Payload = new();
         }
