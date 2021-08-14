efa91e320420273d1bf87d7302f6bc3d8fb32d3d
Nick Kennedy <nickkennedy334@gmail.com>
2021-08-14 14:33:43 -0400

    fix: added missing packet classes

---
+++ Currycomb.Common/Network/Game/Packets/PacketClientMovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currycomb.Common.Network.Game.Packets
+{
+    public record PacketClientMovePlayer(double XPos, double YPos, double ZPos, float Yaw, float Pitch, bool OnGround) : IGamePacket
+    {
+        //public async Task<PacketClientMovePlayerPos> ReadAsync(Stream stream) =>
+        //    new(
+        //        stream.ReadDouble(),
+        //        stream.ReadDouble(),
+        //        stream.ReadDouble(),
+        //        stream.ReadSingle(),
+        //        stream.ReadSingle(),
+        //        stream.ReadBoolean());
+    }
+}
---
+++ Currycomb.Common/Network/Game/Packets/PacketClientMovePlayerPos.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Currycomb.Common.Network.Game.Packets
+{
+    public record PacketClientMovePlayerPos(double XPos,  double YPos, double ZPos, float Yaw, float Pitch, bool OnGround) : IGamePacket
+    {
+        //public async Task<PacketClientMovePlayerPos> ReadAsync(Stream stream) =>
+        //    new(
+        //        stream.ReadDouble(),
+        //        stream.ReadDouble(),
+        //        stream.ReadDouble(),
+        //        stream.ReadSingle(),
+        //        stream.ReadSingle(),
+        //        stream.ReadBoolean());
+    }
+}
